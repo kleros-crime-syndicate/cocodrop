c709f85ffe4bbe2919bba8539360cfcefd4300c3
shotaronowhere <shawtarohgn@gmail.com>
2022-10-08 11:44:23 -0500

    feat(redeem): merkle proof

--- contracts/src/Cocodrop.sol
+++ contracts/src/Cocodrop.sol
@@ -1,5 +1,5 @@
 /**
- * @authors: [@greenlucid, @iamnowhere]
+ * @authors: [@greenlucid, @shotaronowhere]
  * @reviewers: []
  * @auditors: []
  * @bounties: []
@@ -10,6 +10,7 @@
 pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/cryptography/MerkleProof.sol";
 
 /**
  * @title Cocodrop
@@ -25,7 +26,7 @@ contract Cocodrop {
   }
 
   event NewAirdrop(uint256 indexed airdropId, bytes32 merkleRoot, IERC20 token, uint256 amount, string ipfs);
-  event Redemption(uint256 indexed airdropId, address receiver, uint256 amount);
+  event Redemption(uint256 indexed airdropId, uint256 amount);
 
   Airdrop[] public airdrops;
   mapping(uint256 => mapping(address => bool)) public redeemed;
@@ -41,14 +42,42 @@ contract Cocodrop {
     emit NewAirdrop(airdrops.length - 1, _merkleRoot, _token, _amount, _ipfs);
   }
 
-  function redeem(uint256 _airdropId, address _receiver, bytes32[] memory _merkleProof) public {
-    // magic proving goes here
-
-    // check the amount is under or equal pending amount by the airdrop
-    redeemed[_airdropId][_receiver] = true;
-    // reduce the amount of the airdrop by the amount redeemed
+  function redeem(uint256 _airdropId, uint256 _claimedAmount, byte32[] memory _merkleProof) public {
+    uint256 airdropBalance = airdrops[_airdropId].amount;
+    // check
+    require(_claimedAmount <= airdropBalance, "Insufficient airdrop balance.");
+    require(!redeemed[_airdropId][msg.sender], "Already redeemed");
+    require(verifyClaim(_airdropId, _claimedAmount, _merkleProof), "Invalid redemption.");
+    // effects
+    redeemed[_airdropId][msg.sender] = true;
+    airdrops[_airdropId].amount -= airdropBalance - _claimedAmount;
     // transfer goes here
-    uint256 amount = 0; // made up, please change
-    emit Redemption(_airdropId, _receiver, amount);
+    airdrop.token.transfer(msg.sender, _claimedAmount);
+    emit Redemption(_airdropId, amount);
   }
+
+    /**
+     * @notice Verifies a claim.
+     * @param _claimedBalance The amount being claimed.
+     * @param _merkleProof The merkle proof for the claim, sorted from the leaf to the root of the tree.
+     * @param _airdropId The id of the airdrop.
+     */
+    function verifyClaim(
+        uint256 _airdropId
+        uint _claimedAmount,
+        bytes32[] memory _merkleProof
+    )
+        public
+        view
+        returns (bool valid)
+    {
+      bytes32 leaf;
+      assembly {
+        // efficient hash abi.encode(msg.sender, _claimedAmount)
+        mstore(0x00, msg.sender)
+        mstore(0x20, _claimedAmount)
+        leaf := keccak256(0x00, 0x40)
+      }
+      return MerkleProof.verify(_merkleProof, airdrops[_airdropId].merkleRoot, leaf);
+    }
 }
