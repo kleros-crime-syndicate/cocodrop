2fa1cdac272cd5c1d3c598fff621f4e2d4829fe9
Green <40367733+greenlucid@users.noreply.github.com>
2022-10-08 11:20:42 -0500

    feat(subgraph): templates

--- subgraph/README.md
+++ subgraph/README.md
@@ -1,1 +1,12 @@
-# cocodrop
+# cocodrop subgraph
+
+### How to deploy to a network
+
+You'll have to `npx graph auth ...` somewhere.
+
+1. update the `networks.json` with the new contract, and probably add the network -> networkId meaning to   `render-templates.js`
+2. `node render-templates.js <network>
+3. `npx graph codegen`
+4. `yarn fix-schema.ts`
+5. `npx graph build`
+6. `npx graph deploy --product hosted-service <user>/<subgraph-name>`
---
+++ subgraph/render-templates.js
@@ -0,0 +1,31 @@
+const fs = require("fs-extra");
+const mustache = require("mustache");
+
+const chainNameToChainId = {
+  xdai: 100,
+  mainnet: 1,
+  goerli: 5,
+};
+
+async function main() {
+  const networkName = process.argv[2];
+  const chainId = chainNameToChainId[networkName];
+  const deployments = JSON.parse(fs.readFileSync("networks.json", "utf8"));
+  const { address, startBlock } = deployments["Cocodrop"][chainId];
+
+  const templateData = {
+    network: networkName,
+  };
+  templateData["Cocodrop"] = {
+    address: address,
+    addressLowerCase: address.toLowerCase(),
+    startBlock: startBlock,
+  };
+
+  for (const templatedFileDesc of [["subgraph", "yaml"]]) {
+    const template = fs.readFileSync(`${templatedFileDesc[0]}.template.${templatedFileDesc[1]}`).toString();
+    fs.writeFileSync(`${templatedFileDesc[0]}.${templatedFileDesc[1]}`, mustache.render(template, templateData));
+  }
+}
+
+main();
