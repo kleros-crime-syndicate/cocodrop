c380aee5e17d744cbc890073f160dbacbfc6d669
jaybuidl <jb@kleros.io>
2022-10-08 21:14:21 -0500

    feat: erc20 metadata

--- contracts/src/Cocodrop.sol
+++ contracts/src/Cocodrop.sol
@@ -11,6 +11,7 @@
 pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
 /**
--- contracts/typechain-types/index.ts
+++ contracts/typechain-types/index.ts
@@ -6,6 +6,8 @@ export type { openzeppelin };
 import type * as src from "./src";
 export type { src };
 export * as factories from "./factories";
+export type { IERC20Metadata } from "./@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata";
+export { IERC20Metadata__factory } from "./factories/@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata__factory";
 export type { IERC20 } from "./@openzeppelin/contracts/token/ERC20/IERC20";
 export { IERC20__factory } from "./factories/@openzeppelin/contracts/token/ERC20/IERC20__factory";
 export type { Cocodrop } from "./src/Cocodrop";
