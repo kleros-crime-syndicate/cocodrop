6ddff229ab07e6e7794b58b5b092a1590db0d7ea
Green <40367733+greenlucid@users.noreply.github.com>
2022-10-08 19:24:50 -0500

    feat(frontend): claims as a dictionary

--- interface/src/strategies/generateMerkle.ts
+++ interface/src/strategies/generateMerkle.ts
@@ -1,50 +1,52 @@
-import pohStrategy from './poh';
+import pohStrategy from "./poh";
 import getRewards from "./getRewards";
 import { BigNumber } from "ethers";
 import { MerkleTree } from "@kleros/merkle-tree";
 
 export interface Merkle {
   claims: {
-    proof: string[];
-    node: string;
-    address: string;
-    amount: BigNumber;
-  }[];
+    [address: string]: {
+      proof: string[];
+      node: string;
+      address: string;
+      amount: BigNumber;
+    };
+  };
   root: string;
   width: number;
   height: number;
-
 }
 
 const generateMerkle = async (totalAmount: BigNumber): Promise<Merkle> => {
-    const shares = await pohStrategy.computeShares();
+  const shares = await pohStrategy.computeShares();
 
-    const rewards = getRewards(totalAmount,shares.totalWeight, shares.shares);
-    const nodedRewards = rewards.map(item => ({ ...item,
-      node: MerkleTree.makeLeafNode(item.address,item.amount as any)
-        
-    }))
-    const nodes = nodedRewards.map(item => item.node)
-    console.log(rewards[0].address);
-    console.log(rewards[0].amount);
-    console.log(nodes[0]);
-    const mt = new MerkleTree(nodes);
+  const rewards = getRewards(totalAmount, shares.totalWeight, shares.shares);
+  const nodedRewards = rewards.map((item) => ({
+    ...item,
+    node: MerkleTree.makeLeafNode(item.address, item.amount as any),
+  }));
+  const nodes = nodedRewards.map((item) => item.node);
+  const mt = new MerkleTree(nodes);
 
-    const claims = nodedRewards.map(
-      (nodedReward) => ({
-        ...nodedReward,
-        proof: mt.getHexProof(nodedReward.node),
-      }),
-    );
+  const claims = nodedRewards.map((nodedReward) => ({
+    ...nodedReward,
+    proof: mt.getHexProof(nodedReward.node),
+  }));
 
-    const merkleTree = {
-      claims,
-      root: mt.getHexRoot(),
-      width: mt.getWidth(),
-      height: mt.getHeight(),
-    };
+  const keyedClaims = {};
+  claims.forEach((claim) => {
+    keyedClaims[claim.address] = { ...claim };
+    delete keyedClaims[claim.address].address;
+  });
 
-    return (merkleTree);
-}
+  const merkleTree = {
+    claims: keyedClaims,
+    root: mt.getHexRoot(),
+    width: mt.getWidth(),
+    height: mt.getHeight(),
+  };
 
-export default generateMerkle
+  return merkleTree;
+};
+
+export default generateMerkle;
