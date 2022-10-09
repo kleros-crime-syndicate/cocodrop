f3096b7fdf6f2e146a08246cc823774d0ea3c559
shotaronowhere <shawtarohgn@gmail.com>
2022-10-09 02:47:30 -0500

    feat(strategy): sismo (ZKPOH 10000009)

--- interface/src/strategies/index.ts
+++ interface/src/strategies/index.ts
@@ -2,5 +2,6 @@ import pohStrategy from "./poh";
 import lensStrategy from "./lens";
 import poapStrategy from "./poap";
 import snapshotBriber from "./snapshotBriber";
+import sismoStrategy from "./sismo";
 
-export default [pohStrategy, lensStrategy, poapStrategy, snapshotBriber];
+export default [pohStrategy, lensStrategy, poapStrategy, snapshotBriber, sismoStrategy];
---
+++ interface/src/strategies/sismo.ts
@@ -0,0 +1,57 @@
+import request from "graphql-request";
+import { Strategy } from "types";
+import { BigNumber } from "ethers";
+
+const getBatch = async (first: number, skip: number, tokenId: number): Promise<string[]> => {
+  const query = `{
+    sbadges(first: ${first}, skip: ${skip}, where: {tokenId: ${tokenId}} ){
+      mintedTo
+    }
+  }`;
+  const result = await request(
+    "https://api.thegraph.com/subgraphs/name/shotaronowhere/sismo",
+    query
+  );
+  return result.sbadges.map((submission: any) => submission.mintedTo);
+};
+
+const getAllSBadges = async (tokenId: number): Promise<string[]> => {
+  const batches = [];
+  let skip = 0;
+  while (true) {
+    const humans = await getBatch(1000, skip, tokenId);
+    batches.push(humans);
+    if (humans.length < 1000) break;
+    skip += 1000;
+  }
+  return batches.flat(1);
+};
+
+const sismoCompute = async (tokenId: number) => {
+  const sbadges = await getAllSBadges(tokenId);
+  const weightedSBadges = sbadges.reduce((acc, human) => ({ ...acc, [human]: 1 }), {} as Record<string, number>);
+  return {
+    totalWeight: sbadges.length,
+    shares: weightedSBadges,
+  };
+};
+
+const getDisplayName = async (tokenId: string) => {
+  const tokenIdHex: string = BigNumber.from(tokenId).toHexString().substring(2);
+  const paddedTokenId: string = "0".repeat(64-tokenIdHex.length)+tokenIdHex;
+  const query = `https://hub.sismo.io/badges/polygon/${paddedTokenId}.json`;
+  const result = await fetch(query);
+  const resultJSON =  await result.json();
+  return "Sismo Badge holders of " + resultJSON.name;
+}
+
+const sismoStrategy: Strategy = {
+  name: "Sismo",
+  description: "Every sismo badge gets an equal share",
+  logoUri: "https://sismo-prod-hub-static.s3.eu-west-1.amazonaws.com/badges/sismo_early_users.svg",
+  parameters: [],
+  computeShares: sismoCompute,
+  getDisplayName,
+};
+
+export default sismoStrategy;
