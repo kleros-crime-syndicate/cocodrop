540872d443fe8cdcfc1045501204a7c3f911e037
alcercu <alcercu@alumni.upv.es>
2022-10-08 21:50:49 -0500

    fix: strategies

--- interface/src/strategies/index.ts
+++ interface/src/strategies/index.ts
@@ -1,14 +1,15 @@
 import pohStrategy from "./poh";
-import generateMerkle from './generateMerkle';
-import publishMerkle from './publishMerkle';
-import { BigNumber } from "ethers";
-import { Merkle } from './generateMerkle';
-import { Metadata } from './publishMerkle';
+// import generateMerkle from './generateMerkle';
+// import publishMerkle from './publishMerkle';
+// import { BigNumber } from "ethers";
+// import { Merkle } from './generateMerkle';
+// import { Metadata } from './publishMerkle';
 
-const mt: Merkle = await generateMerkle(BigNumber.from(100_000_000_000),pohStrategy);
-const meta: Metadata = {
-    title: "Fairdrop",
-    description: "All POH humans"
-}
-const contentId = await publishMerkle(mt,meta);
-console.log(contentId);
+// const mt: Merkle = await generateMerkle(BigNumber.from(100_000_000_000),pohStrategy);
+// const meta: Metadata = {
+//     title: "Fairdrop",
+//     description: "All POH humans"
+// }
+// const contentId = await publishMerkle(mt,meta);
+// console.log(contentId);
+export default [pohStrategy];
