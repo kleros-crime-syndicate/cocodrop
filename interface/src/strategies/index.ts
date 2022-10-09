e86d5abaf284b59d654421052b1856953abb49c0
shotaronowhere <shawtarohgn@gmail.com>
2022-10-08 20:31:36 -0500

    feat(index): test

--- interface/src/strategies/index.ts
+++ interface/src/strategies/index.ts
@@ -1,3 +1,14 @@
 import pohStrategy from "./poh";
+import generateMerkle from './generateMerkle';
+import publishMerkle from './publishMerkle';
+import { BigNumber } from "ethers";
+import { Merkle } from './generateMerkle';
+import { Metadata } from './publishMerkle';
 
-export default [pohStrategy];
+const mt: Merkle = await generateMerkle(BigNumber.from(100_000_000_000),pohStrategy);
+const meta: Metadata = {
+    title: "Fairdrop",
+    description: "All POH humans"
+}
+const contentId = await publishMerkle(mt,meta);
+console.log(contentId);
